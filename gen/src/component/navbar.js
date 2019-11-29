962eaeabb6ab1f2132438ec9165cb09512b365cd
caydon <caydonstoffels7@gmail.com>
2019-11-30 01:34:09 +0200

    A

---
+++ gen/src/component/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/navbar.scss'
+export default function Navbar(){
+    return(
+        <div className="navbar-container">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Purchase</li>|
+                <li>Login</li>
+            </ul>
+        </div>
+    )
+}
